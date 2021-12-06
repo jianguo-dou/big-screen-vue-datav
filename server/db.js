646d4d87c8a45eafb7d3fc4d99e6570412516a61
jzhou <jiezhou0805@outlook.com>
2021-12-06 13:04:02 +0800

    初次提交

---
+++ server/db.js
@@ -0,0 +1,11 @@
+// 数据库连接配置db.js
+module.exports = {
+    mysql: {
+    host: '127.0.0.1',//mysql连接ip地址
+    user: 'root',
+    password: '123456',//mySql用户名密码
+    database: 'CPS_test',//mySql数据库名
+    port: '3306'//mysql连接端口
+    }
+  }
+  
